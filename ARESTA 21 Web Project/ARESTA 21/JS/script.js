2aca6de9c07ee795a6374adabc7add137c78ea91
はる <137861934+hafizzuraman@users.noreply.github.com>
2025-07-06 08:03:43 +0700

    update terbaru

--- ARESTA 21 Web Project/ARESTA 21/JS/script.js
+++ ARESTA 21 Web Project/ARESTA 21/JS/script.js
@@ -1,11 +1,27 @@
 // Initialize Swiper
 new Splide( '.splide', {
     type: 'loop',
-    perPage: 4,             // Tampilkan 3 slide sekaligus
+    perPage: 3,             // Tampilkan 3 slide sekaligus
     focus: 'center',        // Slide aktif di tengah
     gap: '1rem',            // Jarak antar slide
-    autoplay: true,
     padding: '2rem',        // Tambahkan padding supaya sisi kiri/kanan ada space
+    arrows: true, 
+    autoplay: true,
+    delay: 3000,         // Tampilkan tombol navigasi
+    pagination: {
+        el: '.splide__pagination',
+        clickable: true,     // Tombol pagination bisa diklik
+    },
+    breakpoints: {
+        600: {
+            perPage: 1,      // Tampilkan 1 slide pada layar kecil
+            padding: '1rem', // Kurangi padding pada layar kecil
+        },
+        900: {
+            perPage: 2,      // Tampilkan 2 slide pada layar menengah
+            padding: '1.5rem', // Kurangi padding pada layar menengah
+        }
+    }// Aktifkan autoplay // Kecepatan autoplay dalam milidetik
 }).mount();
 
 
